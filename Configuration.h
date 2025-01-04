d774771fc8df92168673e84d8b6c166ca8b0d3c7
Volodymyr Ryzhkov <waryzhkov@gmail.com>
2025-01-04 11:56:45 +0200

    feat: changed default bed pid number

--- Configuration.h
+++ Configuration.h
@@ -776,10 +776,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 155.00
-  #define DEFAULT_bedKi 28
-  #define DEFAULT_bedKd 576
-
+  #define DEFAULT_bedKp 89.59
+  #define DEFAULT_bedKi 12.87
+  #define DEFAULT_bedKd 415.87
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
   //#define BED_LIMIT_SWITCHING   // Keep the bed temperature within BED_HYSTERESIS of the target
